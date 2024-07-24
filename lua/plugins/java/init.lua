7c7179f280bb2b69f00d56b6c3ac442137b6c896
Tim Jungk <tim.jungk@set.de>
2024-07-24 13:23:29 +0200

    Init

---
+++ lua/plugins/java/init.lua
@@ -0,0 +1,47 @@
+return {
+  "nvim-java/nvim-java",
+  config = false,
+  dependencies = {
+    {
+      "neovim/nvim-lspconfig",
+      opts = {
+        servers = {
+          jdtls = {
+            settings = {
+              java = {
+                configuration = {
+                  runtimes = {
+                    {
+                      name = "JavaSE-8",
+                      path = "/home/tj/Source/MonoRepo/program/java8",
+                    },
+                    {
+                      name = "JavaSE-11",
+                      path = "/home/tj/Source/MonoRepo/program/openjdk11",
+                    },
+                  },
+                },
+              },
+            },
+          },
+        },
+        setup = {
+          jdtls = function()
+            require("java").setup({
+              root_markers = {
+                "settings.gradle",
+                "settings.gradle.kts",
+                "pom.xml",
+                "build.gradle",
+                "mvnw",
+                "gradlew",
+                "build.gradle",
+                "build.gradle.kts",
+              },
+            })
+          end,
+        },
+      },
+    },
+  },
+}
